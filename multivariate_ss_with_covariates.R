29ded59936eb2df6107223f22fa6b7f235095b5f
hypselodoris <natespin@gmail.com>
2018-02-15 14:57:25 -0800

    Add files via upload

---
+++ multivariate_ss_with_covariates.R
@@ -0,0 +1,9 @@
+# Fish 507: Applied Time-Series Analysis in Fisheries and Environmental Sciences
+# Week 4 Problems
+
+# Load utility functions
+source("/Volumes/workspaces/nate/public/coding_work/R/functions.R")
+# Load packages
+packages <- c("MARSS", "stats", "forecast", "datasets", "coda", "R2jags", "maps")
+ipak(packages)
+
